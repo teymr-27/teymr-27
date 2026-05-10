16f0aebdeb68b5dcce44a8ef8e55e30f9f573060
teymr-27 <teymr27@gmail.com>
2026-05-10 14:16:11 -0600

    Add files via upload
    Primer proyecto en Java utilizando variables int, impresión de texto y operaciones básicas.

---
+++ Demo01/src/demo01/Demo01.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo01;
+
+/**
+ *
+ * @author teymr
+ */
+public class Demo01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Este es mi primer comentario 
+        /*Esta es una prueba de comentarios
+          que se estaran haciendo en la aplicacion      
+          Inicial*/
+        
+
+    int mNumero1=415; 
+    int mNumero2=542;
+    int mResultado = mNumero1 + mNumero2;
+    System.out.println(mResultado);      
+        
+    int mNumero3= 25; 
+    int mNumero4=30;
+    mResultado = mNumero3 + mNumero4;
+    System.out.println(mResultado);
+       
+    int mNumero5= 50; 
+    int mNumero6= 100;
+    mResultado = mNumero5 * mNumero6;
+    System.out.println(mResultado);
+
+    float mNumero7= 200;
+    float mNumero8= 52; 
+    float mResultado2 = mNumero7 / mNumero8;
+    System.out.println(mResultado2); 
+    
+    String mNumero9 = "51";
+    String mNumero10 = "100";
+    String mResultado3= mNumero9 + mNumero10;
+    System.out.println(mResultado3);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
+
+    
+    
+       
+        
+       
+       
+       
+       
