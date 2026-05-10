8134543e2eb22fe72ca2d507106eb97ec6c21f3f
teymr-27 <teymr27@gmail.com>
2026-05-10 14:23:15 -0600

    Add files via upload
    proyecto Demo02 con operaciones matemáticas, funciones y validaciones en Java

---
+++ Demo02/src/demo02/Demo02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo02;
+
+/**
+ *
+ * @author teymr
+ */
+public class Demo02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPantallaPrincipal mPantalla = new frmPantallaPrincipal();
+        mPantalla.setVisible(true);
+    }
+    
+}
